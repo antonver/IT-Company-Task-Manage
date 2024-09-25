c5dc830e094229ab247e34a62f54e9be45d53b68
antonver <anton.verbovyi@gmail.com>
2024-09-25 22:19:40 +0200

    view file changed, index function was implemented

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from manager.models import Task, Team, Worker
+
+
 # Create your views here.
+@login_required
+def index(request):
+    num_tasks = Task.objects.count()
+    num_teams = Team.objects.count()
+    num_workers = Worker.objects.count()
+    context = {
+        "num_tasks": num_tasks,
+        "num_teams": num_teams,
+        "num_workers": num_workers,
+    }
+    render(request, template_name="pages/index.html", context=context)
