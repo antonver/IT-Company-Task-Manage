fa494b71ce347f39a8fb7a5cb79dd78cc25e0fd6
antonver <anton.verbovyi@gmail.com>
2024-09-25 22:48:09 +0200

    index template_name changed

--- manager/views.py
+++ manager/views.py
@@ -5,7 +5,7 @@ from manager.models import Task, Team, Worker
 
 
 # Create your views here.
-@login_required
+#@login_required
 def index(request):
     num_tasks = Task.objects.count()
     num_teams = Team.objects.count()
@@ -15,4 +15,4 @@ def index(request):
         "num_teams": num_teams,
         "num_workers": num_workers,
     }
-    render(request, template_name="pages/index.html", context=context)
+    return render(request, template_name="manager/index.html", context=context)
