5e08173bc7f66faca08a738c920dff6b6ce3acb1
antonver <anton.verbovyi@gmail.com>
2024-10-17 19:03:44 +0200

    feat(forms): improve form structure and styling
    - Improve the structure and styling of forms by using more concise and readable code.
    - Update form field attributes for better consistency and user experience.
    - Add more descriptive labels and placeholders to enhance clarity.
    - Refactor form code to improve readability and maintainability.

--- manager/forms.py
+++ manager/forms.py
@@ -2,8 +2,14 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm, UsernameField, \
-    PasswordResetForm, SetPasswordForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    PasswordChangeForm,
+    UsernameField,
+    PasswordResetForm,
+    SetPasswordForm,
+)
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
@@ -13,10 +19,11 @@ from manager.models import Task, Worker, Project, TaskType, Position, Team
 class TaskForm(forms.ModelForm):
     assignees = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+        widget=forms.SelectMultiple(attrs={"class": "form-control"}),
     )
-    deadline = forms.DateField(widget=forms.DateInput(attrs={'type': 'date',
-                                                             'class': 'datepicker'}))
+    deadline = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date", "class": "datepicker"})
+    )
 
     class Meta:
         model = Task
@@ -27,32 +34,54 @@ class TaskForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if user:
             self.fields["assignees"].queryset = get_user_model().objects.all()
-            self.fields["assignees"].label_from_instance = lambda obj: f"{obj} {'(Me)' if obj.id == user.id else ''}"
+            self.fields["assignees"].label_from_instance = (
+                lambda obj: f"{obj} {'(Me)' if obj.id == user.id else ''}"
+            )
 
 
 class WorkerForm(UserCreationForm):
-    tasks = forms.ModelMultipleChoiceField(queryset=Task.objects.all(),
-                                           widget=forms.SelectMultiple({'class': 'form-control'}))
+    tasks = forms.ModelMultipleChoiceField(
+        queryset=Task.objects.all(),
+        widget=forms.SelectMultiple({"class": "form-control"}),
+    )
     position = forms.ModelChoiceField(queryset=Position.objects.all())
 
     class Meta(UserCreationForm.Meta):
         model = Worker
-        fields = UserCreationForm.Meta.fields + ("email", "first_name", "last_name",)
+        fields = UserCreationForm.Meta.fields + (
+            "email",
+            "first_name",
+            "last_name",
+        )
 
 
 class TaskFilterForm(forms.Form):
     is_completed = forms.BooleanField(required=False, label="Is completed?")
-    priority = forms.ChoiceField(choices=Task.PRIORITY_CHOICES, required=False, label="Priority")
-    project = forms.ModelChoiceField(required=False, queryset=Project.objects.all(), label="Project",
-                                     empty_label="All Projects")
-    task_type = forms.ModelChoiceField(required=False, queryset=TaskType.objects.all(), label="Task Type",
-                                       empty_label="Any Task Type")
-    assignees = forms.ModelChoiceField(required=False, queryset=get_user_model().objects.all(), label="Assignees",
-                                       empty_label="assignees")
+    priority = forms.ChoiceField(
+        choices=Task.PRIORITY_CHOICES, required=False, label="Priority"
+    )
+    project = forms.ModelChoiceField(
+        required=False,
+        queryset=Project.objects.all(),
+        label="Project",
+        empty_label="All Projects",
+    )
+    task_type = forms.ModelChoiceField(
+        required=False,
+        queryset=TaskType.objects.all(),
+        label="Task Type",
+        empty_label="Any Task Type",
+    )
+    assignees = forms.ModelChoiceField(
+        required=False,
+        queryset=get_user_model().objects.all(),
+        label="Assignees",
+        empty_label="assignees",
+    )
 
     def clean_priority(self):
         priority = self.cleaned_data.get("priority")
-        if priority == '':
+        if priority == "":
             return None  # Handle empty string by returning None
         return priority
 
@@ -66,14 +95,30 @@ class WorkerSearchForm(forms.Form):
 
 
 class WorkerFilterForm(forms.Form):
-    position = forms.ModelChoiceField(queryset=Position.objects.all(), label="position", empty_label="Position", required=False)
-    team = forms.ModelChoiceField(queryset=Team.objects.all(), label="team", empty_label="Team", required=False)
-    projects = forms.ModelChoiceField(queryset=Project.objects.all(), label="projects", empty_label="Projects", required=False)
+    position = forms.ModelChoiceField(
+        queryset=Position.objects.all(),
+        label="position",
+        empty_label="Position",
+        required=False,
+    )
+    team = forms.ModelChoiceField(
+        queryset=Team.objects.all(), label="team", empty_label="Team", required=False
+    )
+    projects = forms.ModelChoiceField(
+        queryset=Project.objects.all(),
+        label="projects",
+        empty_label="Projects",
+        required=False,
+    )
 
 
 class TeamForm(forms.ModelForm):
-    workers = forms.ModelMultipleChoiceField(queryset=Worker.objects.all(), label="Workers")
-    projects = forms.ModelMultipleChoiceField(queryset=Project.objects.all(), label="Projects")
+    workers = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.all(), label="Workers"
+    )
+    projects = forms.ModelMultipleChoiceField(
+        queryset=Project.objects.all(), label="Projects"
+    )
 
     class Meta:
         fields = "__all__"
@@ -84,12 +129,18 @@ class TeamForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if user:
             self.fields["workers"].queryset = get_user_model().objects.all()
-            self.fields["workers"].label_from_instance = lambda obj: f"{obj} {'(Me)' if obj.id == user.id else ''}"
+            self.fields["workers"].label_from_instance = (
+                lambda obj: f"{obj} {'(Me)' if obj.id == user.id else ''}"
+            )
 
 
 class TeamFilterForm(forms.Form):
-    projects = forms.ModelChoiceField(queryset=Project.objects.all(), label="projects",
-                                      empty_label="Projects", required=False)
+    projects = forms.ModelChoiceField(
+        queryset=Project.objects.all(),
+        label="projects",
+        empty_label="Projects",
+        required=False,
+    )
 
 
 class TeamSearchForm(forms.Form):
@@ -97,8 +148,9 @@ class TeamSearchForm(forms.Form):
 
 
 class ProjectForm(forms.ModelForm):
-    deadline = forms.DateField(widget=forms.DateInput(attrs={'type': 'date',
-                                                             'class': 'datepicker'}))
+    deadline = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date", "class": "datepicker"})
+    )
     tasks = forms.ModelMultipleChoiceField(queryset=Task.objects.all(), label="tasks")
 
     class Meta:
@@ -107,11 +159,21 @@ class ProjectForm(forms.ModelForm):
 
 
 class ProjectFilterForm(forms.Form):
-    deadline = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'datepicker'}), required=False)
-    participants = forms.ModelMultipleChoiceField(queryset=Worker.objects.all(), label="Participants",
-                                                  widget=forms.SelectMultiple(attrs={'class': 'form-control'}), required=False)
-    team = forms.ModelMultipleChoiceField(queryset=Team.objects.all(),
-                                           widget=forms.SelectMultiple(attrs={'class': 'form-control'}), required=False)
+    deadline = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date", "class": "datepicker"}),
+        required=False,
+    )
+    participants = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.all(),
+        label="Participants",
+        widget=forms.SelectMultiple(attrs={"class": "form-control"}),
+        required=False,
+    )
+    team = forms.ModelMultipleChoiceField(
+        queryset=Team.objects.all(),
+        widget=forms.SelectMultiple(attrs={"class": "form-control"}),
+        required=False,
+    )
 
 
 class ProjectSearchForm(forms.Form):
@@ -122,69 +184,100 @@ class RegistrationForm(UserCreationForm):
     model = Worker
     password1 = forms.CharField(
         label=_("Password"),
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        ),
     )
     password2 = forms.CharField(
         label=_("Confirm Password"),
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm Password"}
+        ),
     )
 
     class Meta:
         model = User
-        fields = ('username', 'email',)
+        fields = (
+            "username",
+            "email",
+        )
 
         widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Username'
-            }),
-            'email': forms.EmailInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Email'
-            }),
-            "position": forms.ModelChoiceField(queryset=get_user_model().objects.select_related("position"),
-                                               label="Position"
-                                               )
+            "username": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Username"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"class": "form-control", "placeholder": "Email"}
+            ),
+            "position": forms.ModelChoiceField(
+                queryset=get_user_model().objects.select_related("position"),
+                label="Position",
+            ),
         }
 
 
 class LoginForm(AuthenticationForm):
     model = Worker
-    username = UsernameField(label=_("Your Username"),
-                             widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
+    username = UsernameField(
+        label=_("Your Username"),
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Username"}
+        ),
+    )
     password = forms.CharField(
         label=_("Your Password"),
         strip=False,
-        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        ),
     )
 
 
 class UserPasswordResetForm(PasswordResetForm):
     model = Worker
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Email'
-    }))
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Email"})
+    )
 
 
 class UserSetPasswordForm(SetPasswordForm):
     model = Worker
-    new_password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'New Password'
-    }), label="New Password")
-    new_password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Confirm New Password'
-    }), label="Confirm New Password")
+    new_password1 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "New Password"}
+        ),
+        label="New Password",
+    )
+    new_password2 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm New Password"}
+        ),
+        label="Confirm New Password",
+    )
 
 
 class UserPasswordChangeForm(PasswordChangeForm):
     model = Worker
-    old_password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Old Password'
-    }), label='Old Password')
-    new_password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'New Password'
-    }), label="New Password")
-    new_password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Confirm New Password'
-    }), label="Confirm New Password")
+    old_password = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Old Password"}
+        ),
+        label="Old Password",
+    )
+    new_password1 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "New Password"}
+        ),
+        label="New Password",
+    )
+    new_password2 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm New Password"}
+        ),
+        label="Confirm New Password",
+    )
