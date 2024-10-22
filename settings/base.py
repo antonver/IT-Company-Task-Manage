7cb7919495d8a06cd58264c5eaa5b9830d89e880
antonver <anton.verbovyi@gmail.com>
2024-10-22 18:13:52 +0200

    fix(settings): Remove default SECRET_KEY
    Ensure SECRET_KEY is always retrieved from the environment.

--- settings/base.py
+++ settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "default_key")
+SECRET_KEY = os.environ["SECRET_KEY"]
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 
